a4a143508910f80fa76445f85d3750688d0e4d7b
navisar <navjotsaran137@gmail.com>
2017-05-24 12:14:32 -0500

    First commit

---
+++ stop_go.c/Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+C_SRCS += \
+../stop_go.c \
+../tm4c123gh6pm_startup_ccs.c 
+
+OBJS += \
+./stop_go.obj \
+./tm4c123gh6pm_startup_ccs.obj 
+
+C_DEPS += \
+./stop_go.pp \
+./tm4c123gh6pm_startup_ccs.pp 
+
+C_DEPS__QUOTED += \
+"stop_go.pp" \
+"tm4c123gh6pm_startup_ccs.pp" 
+
+OBJS__QUOTED += \
+"stop_go.obj" \
+"tm4c123gh6pm_startup_ccs.obj" 
+
+C_SRCS__QUOTED += \
+"../stop_go.c" \
+"../tm4c123gh6pm_startup_ccs.c" 
+
+
